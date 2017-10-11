ecb5b3a91c0c71ee7eb745e20162ff38fb9f5d3f
Anindo Shahriar <31381799+Anindo2008@users.noreply.github.com>
2017-10-11 13:55:12 +0600

    Add files via upload

---
+++ defaultprogram.cpp
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<windows.h>
+
+int main()
+{
+    system("COLOR 3f");
+    int a[20],i,n;
+    printf("how many elements dou u want:");
+    scanf("%d",&n);
+    for(i=0;i<n;i++){
+        printf("enter elements:");
+        scanf("%d",&a[i]);
+    }
+    /*int b;
+    printf("enter new element:");
+    scanf("%d",&b);
+    a[n]=b;*/
+    for(i=0;i<n;i++){
+        printf("%d\n",a[i]);
+    }
+    int b,position;
+    printf("\nenter new element:");
+    scanf("%d",&b);
+    printf("\nenter the position:");
+    scanf("%d",&position);
+    for(i=n;i>=position;i--){
+        a[i]=a[i-1];
+    }
+    a[position-1]=b;
+    n++;
+    printf("\nadded new elements are:");
+    for(i=0;i<n+1;i++){
+        printf("%d\n",a[i]);
+    }
+
+    return 0;
+}
